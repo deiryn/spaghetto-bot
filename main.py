f804366305c4057110dd48ff81074c21354a3d85
dei <allamegod@gmail.com>
2023-03-06 13:59:47 +0300

    ....

--- main.py
+++ main.py
@@ -518,6 +518,7 @@ def snake(update: Update, context: CallbackContext):
 		try:
 			cursor = connection.execute(f"UPDATE achievements SET ach13 = 1 WHERE id = {update.effective_user.id};")
 			print("connection")
+			print(f"UPDATE achievements SET ach13 = 1 WHERE id = {update.effective_user.id};")
 			update.message.reply_text(f"🏆 🐍 🏆")
 			print("\x1b[36mdb\x1b[m \x1b[33mupdated\x1b[m || ACHIEVEMENT ADDED")
 		except Exception as e:
