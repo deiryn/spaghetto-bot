f37a8d5e9ae6a519c25ea07b41ea5d2c56e25fc1
dei <allamegod@gmail.com>
2023-03-06 13:46:23 +0300

    trying to bugfix somethin

--- main.py
+++ main.py
@@ -512,9 +512,12 @@ def snake(update: Update, context: CallbackContext):
 	results = cursor.fetchall()
 	getAchvs = results[0]
 	if getAchvs[13] == 0:
-		cursor = connection.execute(f"UPDATE achievements SET ach13 = 1 WHERE id = {update.effective_user.id}")
-		update.message.reply_text(f"🏆 🐍 🏆")
-		print("\x1b[36mdb\x1b[m \x1b[33mupdated\x1b[m || ACHIEVEMENT ADDED")
+		try:
+			cursor = connection.execute(f"UPDATE achievements SET ach13 = 1 WHERE id = {update.effective_user.id}")
+			update.message.reply_text(f"🏆 🐍 🏆")
+			print("\x1b[36mdb\x1b[m \x1b[33mupdated\x1b[m || ACHIEVEMENT ADDED")
+		except Exception as e:
+			print(e)
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
